e6c1e556ed1cdeaa77a779b9e236fb934f53a05c
Nikola Petrovic <nikolapetrovic@Nikolas-MacBook-Pro.local>
2019-06-19 16:27:29 +0200

    jsons

--- src/main/java/com/nikpetrovic/task/TaskApplication.java
+++ src/main/java/com/nikpetrovic/task/TaskApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TaskApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TaskApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
 
 }
